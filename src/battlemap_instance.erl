15515f36d7bbd53c7049c597ea2c6f942dc39ded
nsensfel <SpamShield0@noot-noot.org>
2017-11-22 15:53:46 +0100

    Working on player turns.

--- src/battlemap_instance.erl
+++ src/battlemap_instance.erl
@@ -4,7 +4,9 @@
    [
       get_char_instances/1,
       get_char_instance/2,
-      set_char_instance/3
+      set_char_instance/3,
+      can_play_char_instance/3,
+      post_play_char_instance/2
    ]
 ).
 
@@ -16,6 +18,55 @@ get_char_instances (BattlemapInstance) ->
       fun ({_K, V}) -> V end,
       dict:to_list(BattlemapInstance#battlemap_instance.chars)
    ).
+
+can_play_char_instance
+(
+   BattlemapInstance,
+   PlayerID,
+   CharInstID
+) ->
+   (
+      (array:get(BattlemapInstance#battlemap_instance.curr_player) =:= PlayerID)
+      and
+      lists:member(CharInstID, BattlemapInstance#battlemap_instance.rem_chars)
+   ).
+
+post_play_char_instance (BattlemapInstance, CharInstID) ->
+   case BattlemapInstance#battlemap_instance.rem_chars of
+      [CharInstID|[]] ->
+         NextPlayer =
+            (
+               (BattlemapInstance#battlemap_instance.curr_player + 1)
+               rem
+               array:size(BattlemapInstance#battlemap_instance.players)
+            ),
+         BattlemapInstance#battlemap_instance
+         {
+            curr_player = NextPlayer,
+            rem_chars =
+               lists:filtermap
+               (
+                  fun ({K, V}) ->
+                     case character_instance:get_owner(V) of
+                        NextPlayer -> {true, K};
+                        _ -> false
+                     end
+                  end,
+                  dict:to_list(BattlemapInstance#battlemap_instance.chars)
+               )
+         };
+
+      _ ->
+         BattlemapInstance#battlemap_instance
+         {
+            rem_chars =
+               lists:delete
+               (
+                  CharInstID,
+                  BattlemapInstance#battlemap_instance.rem_chars
+               )
+         }
+   end.
 
 get_char_instance (BattlemapInstance, CharInstID) ->
    dict:fetch(CharInstID, BattlemapInstance#battlemap_instance.chars).
--- src/timed_cache_data.hrl
+++ src/timed_cache_data.hrl
@@ -1,6 +1,6 @@
 %% TODO: add types.
 -record(battlemap, {id, width, height, content, instances}).
--record(battlemap_instance, {id, chars, curr_player, rem_chars, last_turn}).
+-record(battlemap_instance, {id, chars, curr_player, players, rem_chars, last_turn}).
 -record(character, {id, name, icon, portrait, mov_pts, atk_rg}).
 -record(character_turn, {id, path, target}).
 -record(player, {id, battlemaps, characters}).
