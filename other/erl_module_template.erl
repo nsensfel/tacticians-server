2273ff20faa24a0daba4e4b43ace250716a39172
nsensfel <SpamShield0@noot-noot.org>
2017-11-28 21:07:34 +0100

    Homogenizes the modules' presentation.

--- other/erl_module_template.erl
+++ other/erl_module_template.erl
@@ -1,43 +1,12 @@
--module(battlemap).
+-module(name).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% TYPES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--record
-(
-   battlemap,
-   {
-      id,
-      width,
-      height,
-      content,
-      instances
-   }
-).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% Accessors
--export
-(
-   [
-      get_id/1,
-      get_width/1,
-      get_height/1,
-      list_tiles/1,
-      get_instances/1
-   ]
-).
-
-%%%% Utils
--export
-(
-   [
-      cross/5,
-      dist/2
-   ]
-).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -46,15 +15,3 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% EXPORTED FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%% Accessors
-get_id (Battlemap) -> Battlemap#battlemap.id.
-get_width (Battlemap) -> Battlemap#battlemap.width.
-get_height (Battlemap) -> Battlemap#battlemap.height.
-list_tiles (Battlemap) -> array:sparse_to_list(Battlemap#battlemap.content).
-get_instances (Battlemap) -> Battlemap#battlemap.instances.
-
-%%%% Utils
--include("battlemap/cross_5.erl").
-
-dist ({OX, OY}, {DX, DY}) ->
-   (abs(OX - DX) + abs(OY + DY)).
--- src/type/character_instance.erl
+++ src/type/character_instance.erl
@@ -1,4 +1,8 @@
 -module(character_instance).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TYPES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -record
 (
    character_instance,
@@ -9,44 +13,45 @@
       team
    }
 ).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Accessors
 -export
 (
    [
       get_location/1,
       get_current_health/1,
-      get_owner/1
-   ]
-).
--export
-(
-   [
-      new_instance_of/3
-   ]
-).
--export
-(
-   [
+      get_owner/1,
       set_location/3,
-      mod_health/3,
+      mod_health/3
+   ]
+).
+
+%%%% Utils
+-export
+(
+   [
+      new_instance_of/3,
       is_dead/1 % is_alive is reserved.
    ]
 ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTED FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Accessors
 get_location (CharInst) ->
    {CharInst#character_instance.x, CharInst#character_instance.y}.
 
 get_current_health (CharInst) -> CharInst#character_instance.health.
 
 get_owner (CharInst) -> CharInst#character_instance.team.
-
-new_instance_of (Char, Owner, {X, Y}) ->
-   #character_instance
-   {
-      x = X,
-      y = Y,
-      health = character:get_max_health(Char),
-      team = Owner
-   }.
 
 set_location (CharInst, X, Y) ->
    CharInst#character_instance
@@ -68,5 +73,14 @@ mod_health (CharInst, MaxHealth, HealthMod) ->
          CharInst#character_instance{ health = NewHealth }
    end.
 
-is_dead (CharInst) -> (CharInst#character_instance.health == 0).
+%%%% Utils
+new_instance_of (Char, Owner, {X, Y}) ->
+   #character_instance
+   {
+      x = X,
+      y = Y,
+      health = character:get_max_health(Char),
+      team = Owner
+   }.
 
+is_dead (CharInst) -> (CharInst#character_instance.health == 0).
