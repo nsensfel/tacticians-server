417d83aa3b6c9cbe19cb1e10286c97b6e5301f1c
nsensfel <SpamShield0@noot-noot.org>
2018-01-16 18:21:04 +0100

    Links character w/ glyphs/armors/weapons.

--- src/type/character.erl
+++ src/type/character.erl
@@ -13,7 +13,10 @@
       portrait,
       health,
       mov_pts,
-      atk_rg
+      atk_rg,
+      weapons,
+      glyphs,
+      armors
    }
 ).
 
@@ -30,7 +33,10 @@
       get_portrait/1,
       get_max_health/1,
       get_movement_points/1,
-      get_attack_range/1
+      get_attack_range/1,
+      get_weapons/1,
+      get_armors/1,
+      get_glyphs/1
    ]
 ).
 
@@ -49,3 +55,6 @@ get_portrait (Char) -> Char#character.portrait.
 get_max_health (Char) -> Char#character.health.
 get_movement_points (Char) -> Char#character.mov_pts.
 get_attack_range (Char) -> Char#character.atk_rg.
+get_weapons (Char) -> Char#character.weapons.
+get_armors (Char) -> Char#character.armors.
+get_glyphs (Char) -> Char#character.glyphs.
