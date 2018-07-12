b853df7a1c3efef6b84b90fe8c492611564f8b53
nsensfel <SpamShield0@noot-noot.org>
2018-07-12 17:13:00 +0200

    'tis always better with the missing files.

---
+++ src/battle/struct/btl_map.erl
@@ -0,0 +1,100 @@
+-module(btl_map).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TYPES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-type id() :: binary().
+
+-record
+(
+   map,
+   {
+      id :: id(),
+      width :: integer(),
+      height :: integer(),
+      tile_class_ids :: array:array(btl_tile:class_id())
+   }
+).
+
+-opaque type() :: #map{}.
+
+-export_type([type/0, id/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Accessors
+-export
+(
+   [
+      get_id/1,
+      get_width/1,
+      get_height/1,
+      get_tile_class_ids/1,
+      get_tile_class_id/2
+   ]
+).
+
+-export
+(
+   [
+      from_list/4
+   ]
+).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec location_to_array_index
+   (
+      non_neg_integer(),
+      btl_location:type()
+   )
+   -> ('error' | non_neg_integer()).
+location_to_array_index (ArrayWidth, {X, Y}) ->
+   if
+      (X < 0) -> error;
+      (Y < 0) -> error;
+      (X >= ArrayWidth) -> error;
+      true -> ((Y * ArrayWidth) + X)
+   end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTED FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Accessors
+-spec get_id (type()) -> id().
+get_id (Battlemap) -> Battlemap#map.id.
+
+-spec get_width (type()) -> integer().
+get_width (Battlemap) -> Battlemap#map.width.
+
+-spec get_height (type()) -> integer().
+get_height (Battlemap) -> Battlemap#map.height.
+
+-spec get_tile_class_ids (type()) -> array:array(btl_tile:class_id()).
+get_tile_class_ids (Battlemap) -> Battlemap#map.tile_class_ids.
+
+-spec get_tile_class_id (btl_location:type(), type()) -> btl_tile:class_id().
+get_tile_class_id (Location, Battlemap) ->
+   TileIX = location_to_array_index(Battlemap#map.width, Location),
+   array:get(TileIX, Battlemap#map.tile_class_ids).
+
+-spec from_list
+   (
+      non_neg_integer(),
+      non_neg_integer(),
+      non_neg_integer(),
+      list(non_neg_integer())
+   )
+   -> type().
+from_list (ID, Width, Height, List) ->
+   TileClassIDs = lists:map(fun btl_tile:class_id_from_int/1, List),
+
+   #map
+   {
+      id = list_to_binary(integer_to_list(ID)),
+      width = Width,
+      height = Height,
+      tile_class_ids = array:from_list(TileClassIDs)
+   }.
---
+++ src/map/query/map_load.erl
@@ -0,0 +1,93 @@
+-module(map_load).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% TYPES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-include("../../../include/yaws_api.hrl").
+
+-record
+(
+   input,
+   {
+      player_id :: binary(),
+      session_token :: binary(),
+      map_id :: binary()
+   }
+).
+
+-record
+(
+   query_state,
+   {
+      map :: map_map:type()
+   }
+).
+
+-type input() :: #input{}.
+-type query_state() :: #query_state{}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-export([out/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-spec parse_input (binary()) -> input().
+parse_input (Req) ->
+   JSONReqMap = jiffy:decode(Req, [return_maps]),
+   PlayerID = maps:get(<<"pid">>, JSONReqMap),
+   SessionToken =  maps:get(<<"stk">>, JSONReqMap),
+   MapID = maps:get(<<"mid">>, JSONReqMap),
+
+   #input
+   {
+      player_id = PlayerID,
+      session_token = SessionToken,
+      map_id = MapID
+   }.
+
+-spec fetch_data (input()) -> query_state().
+fetch_data (Input) ->
+   PlayerID = Input#input.player_id,
+   MapID = Input#input.map_id,
+
+   Map = shr_timed_cache:fetch(map_db, PlayerID, MapID),
+
+   #query_state
+   {
+      map = Map
+   }.
+
+-spec generate_reply(query_state()) -> binary().
+generate_reply (QueryState) ->
+   Map = QueryState#query_state.map,
+
+   SetMap = map_set_map:generate(Map),
+   Output = jiffy:encode(SetMap),
+
+   Output.
+
+-spec handle (binary()) -> binary().
+handle (Req) ->
+   Input = parse_input(Req),
+   shr_security:assert_identity
+   (
+      Input#input.player_id,
+      Input#input.session_token
+   ),
+   shr_security:lock_queries(Input#input.player_id),
+   QueryState = fetch_data(Input),
+   shr_security:unlock_queries(Input#input.player_id),
+   generate_reply(QueryState).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% EXPORTED FUNCTIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+out(A) ->
+   {
+      content,
+      "application/json; charset=UTF-8",
+      handle(A#arg.clidata)
+   }.
